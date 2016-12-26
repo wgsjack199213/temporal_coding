ff3ad122941eb1cb48ec1711281821be788900ab
wgsjack199213 <wgsjack199213@yeah.net>
2016-12-26 13:41:47 +0800

    Successfully reproduce Figure 2 in the paper

--- test.py
+++ test.py
@@ -14,7 +14,8 @@ def model(I_0, alpha, theta_0, w, t_input_dirac):
     for t in range(2000):
         thetas.append(theta)
         I = I_0
-        if dirac_flag and t > t_input_dirac:    # The first time cross the t_input_dirac
+        epsilon = -0.00001
+        if dirac_flag and t_input_dirac > epsilon and t > t_input_dirac:    # The first time cross the t_input_dirac
             I += w  # Synaptic currents as Diracs
             dirac_flag = False
         delta_theta = theta_der(theta, alpha, I)
@@ -49,7 +50,7 @@ def test_response():
     I_0 = 0.005
     theta_0 = -np.pi
     w = 0.1
-    alpha = 0.6     # When alpha = 0.6, the cycle is about 40+ ms.
+    alpha = 1.0     # When alpha = 1.0, the cycle is about 40+ ms.
 
 
     #test_relation_alpha_cycle(I_0, theta_0, w)
@@ -57,26 +58,40 @@ def test_response():
 
     def pcr(I_0, theta_0, w, alpha):    # Compute Phase Response Curve
         line = []
-        for t_dirac_input in np.linspace(0, 60):
+
+        t_fire_wo, thetas = model(I_0, alpha, theta_0, w, -1)
+        X, Y = [], []
+        for t_dirac_input in np.linspace(0, 60, 300):
             #print t_dirac_input
             t_fire, thetas = model(I_0, alpha, theta_0, w, t_dirac_input)
-            t_fire_wo, thetas = model(I_0, alpha, theta_0, w, -1)
-
             #plt.plot(thetas)
 
-            #print t_fire
-            #print t_fire - t_input
-            #print t_fire - t_fire_wo
-            line.append(t_fire - t_fire_wo)
+            Y.append(- t_fire + t_fire_wo)
+            X.append(t_dirac_input)
             label=str(w)
-        return line, label
+        #print len(line)
+        return X, Y, label
 
+
+    # Figure 2 Left
     for w in [0.1, 0.01, -0.1, -0.01]:
-        line, label = pcr(I_0, theta_0, w, alpha)
-        plt.plot(line, label=label)
+        X, Y, label = pcr(I_0, theta_0, w, alpha)
+        plt.plot(X, Y, label='w='+label)
     plt.legend()
-    plt.savefig('Response properties of the theta model (I_0>0).png')
+    #plt.savefig('Response properties of the theta model (I_0>0).png')
     plt.show()
+
+    # Figure 2 Right
+    I_0 = -0.005
+    theta_0 = np.arccos((1 + alpha * I_0) / (1 - alpha * I_0)) + 0.0001
+    for w in [0.1, 0.001, -0.001]:
+        X, Y, label = pcr(I_0, theta_0, w, alpha)
+        plt.plot(X, Y, label='w='+label)
+    plt.legend()
+    plt.ylim(-60, 60)
+    #plt.savefig('Response properties of the theta model (I_0<0).png')
+    plt.show()
+
 
 
 
